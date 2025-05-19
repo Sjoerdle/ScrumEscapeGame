85be27da20fe87f1fc7ec16bf8c45166f386fe55
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2025-05-19 12:54:52 +0200

    MonsterLoader klasse + Lege monster klassen

---
+++ src/ScrumGame/src/main/java/Monsters/MixMonster.java
@@ -0,0 +1,19 @@
+package Monsters;
+
+public class MixMonster extends Monster {
+    @Override
+    protected void toonIntroductie() {
+
+    }
+
+    @Override
+    protected void geefOpdracht() {
+
+    }
+
+    @Override
+    protected boolean controleerAntwoord(String antwoord) {
+
+        return false;
+    }
+}
